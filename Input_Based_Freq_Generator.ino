f53f1b21c776428289fbe8e6b171b8b3b2bd4139
ganeshkumarjammu <jammuganeshkumar@gmail.com>
2024-07-11 16:40:49 +0530

    Urray Sv2.0 successfully working

--- Input_Based_Freq_Generator.ino
+++ Input_Based_Freq_Generator.ino
@@ -1,9 +1,12 @@
+//Link to Sheets for this values 
+
 uint8_t IN[6] = {2, 3, 4, 5, 6, 7};
 unsigned long int sig[6] = {50, 100, 200, 500, 1000, 2000}; // Frequencies in kHz
-long delayUS[6] = {0, 0, 0, 0, 0, 0};
-uint8_t inputPin;
-uint8_t OUT = 13;
-
+unsigned int OCR[6]={159,79,39,15,7,3};  //OCR = ((16,000,000)/(2*sig(in hz))-1)
+//long delayUS[6] = {0, 0, 0, 0, 0, 0};
+int inputPin = -1;
+uint8_t OUT = 9; //OUTPUT SIGNAL PIN is 9
+long wait ;
 void setup() {
   for (int i = 0; i < 6; i++) {
     pinMode(IN[i], INPUT_PULLUP);
@@ -11,35 +14,33 @@ void setup() {
   pinMode(OUT, OUTPUT);
   Serial.begin(9600);
   Serial.println("Hello ");
-  findHalfDutyCycle();
+//  findHalfDutyCycle();
 }
 
 void loop() {
   for (int i = 0; i < 6; i++) {
     if (!digitalRead(IN[i])) {
-      inputPin = i;
       Serial.print("Given Input on Pin: ");
       Serial.println(inputPin + 2);
       Serial.print("Generate Signal of kHz: ");
       Serial.println(sig[i]);
-      Serial.print("Generated half duty delay: ");
-      Serial.println(delayUS[i]);      
-      delay(1500);
-      
-      while (1) {
-        digitalWrite(OUT, HIGH);
-        delayMicroseconds(delayUS[i]); 
-        digitalWrite(OUT, LOW);
-        delayMicroseconds(delayUS[i]); 
+//      Serial.print("Generated half duty delay: ");
+//      Serial.println(delayUS[i]);      
+      //delay(1500);
+      if(inputPin!=i){
+        inputPin = i ;
+        TCCR1A = _BV(COM1A0); // Toggle OC1A on Compare Match (PWM mode)
+        TCCR1B = _BV(WGM12) | _BV(CS10);  // CTC mode, no prescaler
+        OCR1A = OCR[i]; // Set OCR1A for 500 kHz frequency at 16 MHz clock
       }
     }
   } 
 }
 
-void findHalfDutyCycle() {
-  for (int i = 0; i < 6; i++) {
-    delayUS[i] = 1000000 / (2 * sig[i] * 1000); // Calculate half period in microseconds
-    Serial.print("Half Delays are: ");
-    Serial.println(delayUS[i]);
-  }
-}
+//void findHalfDutyCycle() {
+//  for (int i = 0; i < 6; i++) {
+//    delayUS[i] = 1000000 / (2 * sig[i] * 1000); // Calculate half period in microseconds
+//    Serial.print("Half Delays are: ");
+//    Serial.println(delayUS[i]);
+//  }
+//}
